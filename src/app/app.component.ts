201a9663da70b329d37756078accee609d3df0d0
Janus Nicon <janusnic@gmail.com>
2017-06-29 15:43:18 +0300

    Added unit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  // templateUrl: './app.component.html',
+  // template: "<h1>Hello Friend</h1>",
+  // template: `
+  //       <h1>{{title}}</h1>
+  //       <h2>My favorite food is: {{myFood}}</h2>
+  //       `,
+//   template: `
+//       <h1>{{title}}</h1>
+//       <h2>My favorite hero is: {{myHero}}</h2>
+//       <p>Heroes:</p>
+//       <ul>
+//         <li *ngFor="let hero of heroes">
+//           {{ hero }}
+//         </li>
+//       </ul>
+//       <p *ngIf="heroes.length > 3">There are many heroes!</p>
+//     `,
+//   styleUrls: ['./app.component.css']
+// })
 export class AppComponent {
-  title = 'app';
+  // title = 'app';
+  // myFood = 'Aple';
+  // title = 'Tour of Heroes';
+  // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  // myHero = this.heroes[0];
 }
